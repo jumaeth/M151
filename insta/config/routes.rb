d2e2525cdc814d71b2fcd05fd47e0fd7aa6e93f3
Marco Jucker <mju104828@iet-gibb.ch>
2019-05-02 08:54:23 +0200

    init

---
+++ insta/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Homeseite
+  root 'pages#home'
+
+  resources :users, only: [:index, :show]
+end
